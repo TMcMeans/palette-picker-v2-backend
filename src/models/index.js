5e20e2ce67908e3439e74c9651608c22ee1b87c1
Tanjie McMeans <tanjmcmeans01@gmail.com>
2019-05-16 14:49:11 -0400

    Create index.js file to connect models to db

---
+++ src/models/index.js
@@ -0,0 +1,21 @@
+import Sequelize from 'sequelize';
+
+const sequelize = new Sequelize(process.env.DATABASE_PROJECT, {
+  dialect: 'postgres'
+});
+
+const models = {
+  Project: sequelize.import('./project'),
+  Palette: sequelize.import('./palette')
+};
+
+//Build relational schema based on associate method in models
+Object.keys(models).forEach(key => {
+  if ('associate' in models[key]) {
+    models[key].associate(models);
+  }
+});
+
+export { sequelize };
+
+export default models;
